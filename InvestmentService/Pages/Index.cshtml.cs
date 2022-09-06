ea4a93a35b60c2db9d2437c425175bd899370df6
Vezral <kimseangtancs@gmail.com>
2022-09-06 22:16:40 +0800

    completed view

--- InvestmentService/Pages/Index.cshtml.cs
+++ InvestmentService/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using InvestmentService.DataAccess.Repositories;
+﻿using InvestmentService.DataAccess.Models;
+using InvestmentService.DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace InvestmentService.Pages
@@ -6,28 +8,43 @@ namespace InvestmentService.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        private readonly ICustomerInfoRepository _customerInfoRepository;
         private readonly IConsultantInfoRepository _consultantInfoRepository;
-        private readonly IDiscretionaryRuleRepository _discretionaryRuleRepository;
 
         public IndexModel(
             ILogger<IndexModel> logger,
-            ICustomerInfoRepository customerInfoRepository,
-            IConsultantInfoRepository consultantInfoRepository,
-            IDiscretionaryRuleRepository discretionaryRuleRepository
+            IConsultantInfoRepository consultantInfoRepository
         )
         {
             _logger = logger;
-            _customerInfoRepository = customerInfoRepository;
             _consultantInfoRepository = consultantInfoRepository;
-            _discretionaryRuleRepository = discretionaryRuleRepository;
         }
+
+        [BindProperty]
+        public List<ConsultantInfo> ConsultantInfos { get; set; }
+        [BindProperty]
+        public int? SelectedConsultantInfoId { get; set; }
+        [BindProperty]
+        public int? SelectedCustomerInfoId { get; set; }
 
         public async Task OnGet()
         {
-            var x = await _customerInfoRepository.GetAsync();
-            var y = await _consultantInfoRepository.GetAsync();
-            var z = await _discretionaryRuleRepository.GetAsync();
+            var consultantInfos = await _consultantInfoRepository.GetAsync();
+            ConsultantInfos = consultantInfos;
+        }
+
+        public async Task OnPostCustomer(int consultantInfoId)
+        {
+            var consultantInfos = await _consultantInfoRepository.GetAsync();
+            ConsultantInfos = consultantInfos;
+            SelectedConsultantInfoId = consultantInfoId;
+        }
+
+        public async Task OnPostDiscretionaryRule(int consultantInfoId, int customerInfoId)
+        {
+            var consultantInfos = await _consultantInfoRepository.GetAsync();
+            ConsultantInfos = consultantInfos;
+            SelectedConsultantInfoId = consultantInfoId;
+            SelectedCustomerInfoId = customerInfoId;
         }
     }
 }
